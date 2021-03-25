6e29f9477d5cf772e7ee4455e0f06eabc9c75c57
AadarshGhodasara <aadarshgh2000@gmail.com>
2021-03-25 19:19:05 +0530

    some changes in header bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,8 @@
 import React , {useState, useEffect} from 'react';
 import './Header.css';
+import './style.css'
+import './responsive.css'
+import './bootstrap.css'
 import { Navbar , Nav  } from 'react-bootstrap';
 import history from '../History';
 import   fire  from '../Firebase';
@@ -121,7 +124,7 @@ function Header() {
                         }
 
                         <Link onSelect={checkExapnd} href="#ContactUs">Contact Us</Link>
-                        { (isAdmin!==true && webStorageAdmin!==true) ?
+                        {isLogin && (isAdmin!==true && webStorageAdmin!==true) ?
                             <Link onSelect={checkExapnd} onClick={handleProductHistoryEvent} href="#">Product History</Link>
                         :
                             null
@@ -129,7 +132,7 @@ function Header() {
                         {isLogin 
                         ? <Link onSelect={checkExapnd} onClick={goLogout} href="#">Logout</Link> 
                         : <Link onSelect={checkExapnd} onClick={goToLogin}>Login</Link>}
-                        {(isAdmin!==true && webStorageAdmin!==true) ?
+                        {isLogin && (isAdmin!==true && webStorageAdmin!==true) ?
                                 <div className='' onClick={GoToCart}>
                                         <ShoppingCartSharpIcon /> 
                                 </div>
@@ -139,6 +142,53 @@ function Header() {
                 </Collapse>
             </Navbar>
         </div>
+    // <div className="header_section">
+    //   <div className="container-fluid">
+    //     <nav className="navbar navbar-expand-lg custom_nav-container">
+    //       <a className="navbar-brand" href="index.html">
+    //         <img src="images/logo.png" alt="" />
+    //       </a>
+    //       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    //         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
+
+    //       <div className="collapse navbar-collapse lg_nav-toggle" id="navbarSupportedContent">
+    //         <ul className="navbar-nav  ">
+    //           <li className="nav-item active">
+    //             <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="about.html"> About</a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="shop.html">Shop </a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="furniture.html"> Furniture </a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="contact.html">Contact us</a>
+    //           </li>
+    //         </ul>
+    //         <div className="user_option">
+    //           <a href="">
+    //             <img src="images/user.png" alt=""/>
+    //             <span>
+    //               Login
+    //             </span>
+    //           </a>
+    //           <form className="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
+    //             <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+    //           </form>
+    //         </div>
+    //       </div>
+    //       <div>
+    //     </div>
+
+    //     </nav>
+    //   </div>
+    // </div>
     )
 }
 
